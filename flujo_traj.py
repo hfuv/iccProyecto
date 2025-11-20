610c06f91f88fbcf24f6a852995ee293888fad3a
MQS144 <mathiusquispesicha123@gmail.com>
2025-11-20 14:24:13 -0500

    Initial commit

---
+++ flujo_traj.py
@@ -0,0 +1,9 @@
+"""
+el flujo de trabajo debe ser de esta manera para poder avanzar correctamente
+-uso de librerias: numpy, cv2,
+-matriz de confusion
+"""
+#-------------------------
+"""
+usare kmeans para la distancia euclideana 
+"""
